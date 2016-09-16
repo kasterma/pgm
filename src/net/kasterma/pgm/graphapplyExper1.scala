b3655eb8729d7e99b3cfd9163fa6c8cc8d77534b
Bart Kastermans <kasterma@kasterma.net>
2016-09-16 07:25:05 +0200

    add rule based node

--- src/net/kasterma/pgm/graphapplyExper1.scala
+++ src/net/kasterma/pgm/graphapplyExper1.scala
@@ -33,6 +33,27 @@ object graphapplyExper1 {
     }
   }
 
+  class Rule(val r: Map[Int, Int], val value: Int) {
+    def test(test_map: Map[Int, Option[Int]]): Boolean =
+      r.forall {case (x,y) => test_map.contains(x) & y == test_map(x).get}
+  }
+
+  class RuleNode(val rules: List[Rule]) extends Node {
+    var inputs: Map[Int, Option[Int]] = Map[Int, Option[Int]]()
+    def set_input(x: Option[Int], idx: Int) = {
+      inputs += idx -> x
+      true
+    }
+    def get_value = {
+      val applicable_rules = rules filter ((rule:Rule) => rule.test(inputs))
+      if (applicable_rules.length != 1) {
+        None
+      } else {
+        Some(applicable_rules.head.value)
+      }
+    }
+  }
+
   case class Edge(from_node: String, to_node: String, to_idx: Int)
 
   class Graph (val nodes: Map[String, Node], val edges: List[Edge]) {
@@ -52,10 +73,13 @@ object graphapplyExper1 {
   val g1 = new Graph(Map("three" -> new ConstantNode(3),
                         "four" -> new ConstantNode(4),
                         "sum" -> new BinaryNode((x,y) => x + y),
-                        "prod" -> new BinaryNode((x,y) => x * y)),
-    List(Edge("three", "sum", 0), Edge("four", "sum", 1), Edge("sum", "prod", 0), Edge("sum", "prod", 1)))
+                        "prod" -> new BinaryNode((x,y) => x * y),
+                        "rule" -> new RuleNode(List(new Rule(Map(3 -> 49), 666)))),
+    List(Edge("three", "sum", 0), Edge("four", "sum", 1), Edge("sum", "prod", 0), Edge("sum", "prod", 1),
+      Edge("prod", "rule", 3)))
 
   println(g1("three"))
   println(g1("sum"))
   println(g1("prod"))
+  println(g1("rule"))
 }
