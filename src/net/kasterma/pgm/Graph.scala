5567834e7128d7721aaf378b3407628c6f42872a
Bart Kastermans <kasterma@kasterma.net>
2016-10-05 07:41:40 +0200

    update evaluation of graph to possibly use externally provided values

--- src/net/kasterma/pgm/Graph.scala
+++ src/net/kasterma/pgm/Graph.scala
@@ -1,5 +1,7 @@
 package net.kasterma.pgm
 
+import java.util.NoSuchElementException
+
 import scala.language.postfixOps
 import scala.annotation.tailrec
 
@@ -11,15 +13,22 @@ object Graph {
 }
 
 class Graph (val nodes: Map[String, Node], val edges: List[Edge]) {
-  def apply(at_node: String): Option[Int] = {
-    val nodeo = nodes.get(at_node)
-    if (nodeo.isDefined) {
-      val node = nodeo.get
-      val parents = edges filter (_.to_node == at_node)
-      parents map (e => node.set_input(apply(e.from_node), e.to_idx))
-      node.get_value
-    } else {
-      None
+  def apply(at_node: String): Option[Int] = apply(at_node, Map.empty)
+
+  def apply(at_node: String, values: Map[String, Int]): Option[Int] = {
+    try {
+      Some(values(at_node))
+    } catch {
+      case e: NoSuchElementException =>
+        val nodeo = nodes.get (at_node)
+        if (nodeo.isDefined) {
+          val node = nodeo.get
+          val parents = edges filter (_.to_node == at_node)
+          parents map (e => node.set_input (apply (e.from_node, values), e.to_idx) )
+          node.get_value
+        } else {
+          None
+        }
     }
   }
 }
