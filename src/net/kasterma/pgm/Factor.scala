3043594083cfe8357ae49c90e682207ca03ef47d
Bart Kastermans <kasterma@kasterma.net>
2016-10-11 07:51:01 +0200

    index2assignment and back added

--- src/net/kasterma/pgm/Factor.scala
+++ src/net/kasterma/pgm/Factor.scala
@@ -11,14 +11,60 @@ object Values {
   def apply(xs: List[Double]): Values = new Values(xs)
 }
 
-class Scope (val name: String, val xs: List[Double]) {
-  def size = xs.length
-}
-
 object Scope {
   def apply(name: String, x: Double, xs: Double*): Scope = new Scope(name, List(x) ++ xs.toList distinct)
   def apply(name: String, xs: List[Double]): Scope = new Scope(name, xs)
 }
+
+class Scope (val name: String, val xs: List[Double]) {
+  def size = xs.length
+
+  override def toString: String = "Scope(" + name + "," + xs.toString() + ")"
+}
+
+class AssignmentError(val message: String) extends Exception
+
+object Assignment {
+  def test_run = {
+    val scopes = List(Scope("hi", 2.6, 1.5), Scope("ho", 2.3, 2.6))
+    var x = Assignment(List(("hi", 2.6), ("ho", 2.3)), scopes)
+    println(x.idx)
+    x = Assignment(List(("hi", 1.5), ("ho", 2.3)), List(Scope("hi", 2.6, 1.5), Scope("ho", 2.3, 2.6)))
+    println(x.idx)
+    x = Assignment(List(("hi", 2.6), ("ho", 2.6)), List(Scope("hi", 2.6, 1.5), Scope("ho", 2.3, 2.6)))
+    println(x.idx)
+    x = Assignment(List(("hi", 1.5), ("ho", 2.6)), List(Scope("hi", 2.6, 1.5), Scope("ho", 2.3, 2.6)))
+    println(x.idx)
+
+  }
+
+  def strides(scopes: List[Scope]): List[Int] = ((scopes dropRight 1) map ((l:Scope) => l.xs.length)).scan(1)(_ * _)
+
+  def apply(assig: List[(String, Double)], scopes: List[Scope]): Assignment = {
+    val assig_map = assig.toMap
+    val assig_idx = scopes map (s => s.xs.indexOf(assig_map(s.name)))
+    if (assig_idx exists (_ < 0)) {
+      throw new AssignmentError("assignment misses a scope")
+    }
+
+    val index = assig_idx zip strides(scopes) map { case (x, stride) => x * stride} sum;
+    new Assignment(assig_map, index)
+  }
+
+  def apply(index: Int, scopes: List[Scope]): Assignment = {
+    val assig_idx = (scopes map (_.xs.length)) zip strides(scopes) map {case (len, stride) => index / stride % len }
+    val assig_map = assig_idx zip scopes map { case (idx, scope) => (scope.name, scope.xs(idx))} toMap;
+    new Assignment(assig_map, index)
+  }
+}
+
+class Assignment (val assig_map: Map[String, Double], val index: Int) {
+  def value(name: String) = assig_map(name)
+  def idx = index
+  override def toString: String = assig_map.toString()
+}
+
+
 
 class FactorError (message: String) extends Exception
 
@@ -55,6 +101,14 @@ class Factor (scopes: List[Scope], values: Values) {
     val new_vals = values.xs.indices  filter (x => (x % stride) / stride2 == within_scope_idx) map {values.xs(_)} toList
 
     Factor(scopes.take(scope_idx) ++ scopes.drop(scope_idx + 1), Values(new_vals))
+  }
+
+  def value(as: Assignment): Double = {
+    42.0
+  }
+
+  def update(as: Assignment, value: Double): Double = {
+    value
   }
 }
 
